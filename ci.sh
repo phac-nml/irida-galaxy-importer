fe9ef2b82ae67076aab4a43b57ee4b37267461ea
Joel Thiessen <joel.thiessen@icloud.com>
2015-04-02 12:03:23 -0500

    Updated CI script

--- ci.sh
+++ ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
 pip install -U bioblend pytest pytest-cov pytest-mock requests-oauthlib subprocess32 splinter
 cd irida_import
 xvfb-run py.test -s
