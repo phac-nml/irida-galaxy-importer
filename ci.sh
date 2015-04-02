3229d025fc99e901fe9696e39cca429496d485ec
Joel Thiessen <joel.thiessen@icloud.com>
2015-04-02 14:58:21 -0500

    Try again

--- ci.sh
+++ ci.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 virtualenv env
 source env/bin/activate
-
 pip install -U bioblend pytest pytest-cov pytest-mock requests-oauthlib subprocess32 splinter
 cd irida_import
 xvfb-run py.test -s
-
 deactivate
