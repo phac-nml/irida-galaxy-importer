1fed2a4f37023c52ab7799f4297e4bd4ece14ffe
Joel Thiessen <joel.thiessen@icloud.com>
2015-04-17 09:25:14 -0500

    Removed ci.sh

--- ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-virtualenv env
-source env/bin/activate
-pip install -U bioblend pytest pytest-cov pytest-mock requests-oauthlib subprocess32 splinter
-cd irida_import
-xvfb-run py.test -s
-deactivate
-
