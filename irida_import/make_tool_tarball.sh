01c1d9ff80811f0e75b0d3625df666a28cdfbbd5
Joel Thiessen <joel.thiessen@icloud.com>
2015-04-21 10:40:04 -0500

    Added Jen's proposal to fix library installation

--- irida_import/make_tool_tarball.sh
+++ irida_import/make_tool_tarball.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp ../README.md README.md
 cp irida_import.xml.sample irida_import.xml
-tar -cvzf ../irida_export_tool.tar.gz README.md irida_import.xml requirements.txt config.ini.sample sample_file.py sample.py tool_dependencies.xml irida_import.py 
+tar -cvzf ../irida_export_tool.tar.gz README.md irida_import.xml config.ini.sample sample_file.py sample.py tool_dependencies.xml irida_import.py 
 rm README.md
