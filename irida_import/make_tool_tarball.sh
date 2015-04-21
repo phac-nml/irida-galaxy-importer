7cc0195ac3bdb54adc477e0f86408c74d32ee27d
Joel Thiessen <joel.thiessen@icloud.com>
2015-04-21 10:16:01 -0500

    Added Eric's archiver changes

--- irida_import/make_tool_tarball.sh
+++ irida_import/make_tool_tarball.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf tests/integration/repos/*
 cp ../README.md README.md
-tar -cvzf ../irida_export_tool.tar.gz * 
+cp irida_import.xml.sample irida_import.xml
+tar -cvzf ../irida_export_tool.tar.gz README.md irida_import.xml requirements.txt config.ini.sample sample_file.py sample.py tool_dependencies.xml irida_import.py 
 rm README.md
