3fc81acce18941679093b99157f961dccfbbffbb
Eric Enns <eric.enns@phac-aspc.gc.ca>
2015-09-01 12:05:33 -0500

    Use selenium webdriver element.click instead of action.click

--- irida_import/tests/integration/test_irida_import_int.py
+++ irida_import/tests/integration/test_irida_import_int.py
@@ -376,12 +376,8 @@ hhhhhhhhhhghhghhhhhfhhhhhfffffe`ee[`X]b[d[ed`[Y[^Y"""
                 el1 = driver.find_element_by_xpath("//table[@id='samplesTable']/tbody/tr[1]/td/div/label")  
                 el2 = driver.find_element_by_xpath("//table[@id='samplesTable']/tbody/tr[2]/td/div/label")
 
-                action = webdriver.common.action_chains.ActionChains(driver)
-                action.move_to_element_with_offset(el1, 5, 5)
-                action.click()
-                action.move_to_element_with_offset(el2, 5, 5)
-                action.click()
-                action.perform()
+                el1.click()
+                el2.click()
 
                 stale = False
             except (StaleElementReferenceException, NoSuchElementException):
