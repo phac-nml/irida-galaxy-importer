3ccbb4b5088aad940e5591327d1d459dec56b375
Eric Enns <eric.enns@phac-aspc.gc.ca>
2015-09-24 15:13:35 -0500

    Update build.sh to use bash -e to exit as soon as one command fails

--- .test-scripts/build.sh
+++ .test-scripts/build.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 virtualenv env
 source env/bin/activate
 pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 requests-oauthlib==0.4.2 subprocess32 selenium
 cd irida_import
 xvfb-run py.test -s
-EXIT_CODE=$?
-pip show bioblend
 deactivate
-exit $EXIT_CODE
