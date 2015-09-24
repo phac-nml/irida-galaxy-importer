331730053f9e05f07aad8938df7caf3d91679670
Eric Enns <eric.enns@phac-aspc.gc.ca>
2015-09-24 14:39:05 -0500

    Return py.test exit code

--- .test-scripts/build.sh
+++ .test-scripts/build.sh
@@ -3,6 +3,8 @@ virtualenv env
 source env/bin/activate
 pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 requests-oauthlib==0.4.2 subprocess32 selenium
 cd irida_import
-xvfb-run py.test -s  
+xvfb-run py.test -s
+EXIT_CODE=$?
 pip show bioblend
-deactivate 
+deactivate
+exit $EXIT_CODE
