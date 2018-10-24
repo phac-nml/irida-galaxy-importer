4d6cb34a41f04018773cbf8498600d58eb154125
takadonet <philipmabon@gmail.com>
2018-10-24 09:45:27 -0500

    increase timeout for IRIDA to launch

--- irida_import/tests/integration/test_irida_import_int.py
+++ irida_import/tests/integration/test_irida_import_int.py
@@ -39,7 +39,7 @@ class TestIridaImportInt:
     must be disabled, in addition to Galaxy starting/stopping
     """
 
-    TIMEOUT = 600  # seconds
+    TIMEOUT = 1200  # seconds
 
     USER = getpass.getuser()
     EMAIL = 'irida@irida.ca'
