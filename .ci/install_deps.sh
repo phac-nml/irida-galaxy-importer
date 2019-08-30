b5b5e77cecd0a627ee7e90af61f887326dbe55a5
Philip.Mabon <philip.mabon@canada.ca>
2019-08-30 07:38:21 -0500

    switching to use conda for dependencies of irida Importer tool and testing platform

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -2,9 +2,27 @@
 
 CHROMEDRIVER_VERSION=$1
 
-python2 -m virtualenv .venv
-source .venv/bin/activate
-pip install -U bioblend pytest pytest-cov pytest-mock requests==2.6 requests-oauthlib==1.2.0 subprocess32 selenium
+#some setup of bioconda 'borrowed' from https://github.com/bioconda/bioconda-recipes/tree/master/.circleci
+WORKSPACE=`pwd`
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $WORKSPACE/miniconda
+
+# step 2: setup channels
+$WORKSPACE/miniconda/bin/conda config --system --add channels defaults
+$WORKSPACE/miniconda/bin/conda config --system --add channels bioconda
+$WORKSPACE/miniconda/bin/conda config --system --add channels conda-forge
+
+
+# Set path
+echo "export PATH=\"$WORKSPACE/miniconda/bin:$PATH\"" >> $BASH_ENV
+source $BASH_ENV
+
+#create conda env as Galaxy would
+conda create -y --quiet --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name irida_importer bioblend=0.13.0 oauthlib=3.0.1 requests=2.22.0 requests-oauthlib=1.2.0 simplejson=3.8.1 python=3.6.7
+
+conda activate irida_importer
+pip install -U pytest pytest-cov pytest-mock selenium
 
 # Install chromedriver if not correct version
 TEST_CHROMEDRIVER_VERSION=`chromedriver --version | sed -e 's/^ChromeDriver //' -e 's/ (.*//' 2>/dev/null`
