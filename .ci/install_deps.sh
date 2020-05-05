d2e1c94691be169008fb0cf32f71eea61e450d1f
Eric Enns <eric.enns@phac-aspc.gc.ca>
2020-05-05 17:21:38 -0500

    Updated install deps with Aarons changes

--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -3,27 +3,37 @@
 CHROMEDRIVER_VERSION=$1
 
 #some setup of bioconda 'borrowed' from https://github.com/bioconda/bioconda-recipes/tree/master/.circleci
-WORKSPACE=`pwd`
-BASH_ENV=`mktemp`
+CONDA_ENV="__irida_importer"
 
-curl -L -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $WORKSPACE/miniconda
+conda --version 2>/dev/null 1>/dev/null
+if [ $? -ne 0 ];
+then
+  echo "Installing conda"
 
-# step 2: setup channels
-$WORKSPACE/miniconda/bin/conda config --system --add channels defaults
-$WORKSPACE/miniconda/bin/conda config --system --add channels bioconda
-$WORKSPACE/miniconda/bin/conda config --system --add channels conda-forge
+  WORKSPACE=`pwd`
+  BASH_ENV=`mktemp`
+
+  curl -L -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  bash miniconda.sh -b -p $WORKSPACE/miniconda
+
+  # Set path
+  echo "export PATH=\"$WORKSPACE/miniconda/bin:$PATH\"" >> $BASH_ENV
+  source $BASH_ENV
+fi
+
+conda list -n $CONDA_ENV 2>/dev/null 1>/dev/null
+if [ $? -ne 0 ];
+then
+  echo "Installing conda environment $CONDA_ENV"
 
 
-# Set path
-echo "export PATH=\"$WORKSPACE/miniconda/bin:$PATH\"" >> $BASH_ENV
-source $BASH_ENV
+  conda create -y --quiet --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name irida_importer bioblend=0.13.0 oauthlib=3.0.1 requests=2.22.0 requests-oauthlib=1.2.0 simplejson=3.8.1 python=3.6.7
 
-#create conda env as Galaxy would
-conda create -y --quiet --override-channels --channel iuc --channel conda-forge --channel bioconda --channel defaults --name irida_importer bioblend=0.13.0 oauthlib=3.0.1 requests=2.22.0 requests-oauthlib=1.2.0 simplejson=3.8.1 python=3.6.7
-
-source activate irida_importer
-pip install -U pytest pytest-cov pytest-mock mock subprocess32 selenium requests_oauthlib requests
+  source activate $CONDA_ENV
+  pip install -U pytest pytest-cov pytest-mock mock subprocess32 selenium
+else
+  source activate $CONDA_ENV
+fi
 
 # Install chromedriver if not correct version
 TEST_CHROMEDRIVER_VERSION=`chromedriver --version | sed -e 's/^ChromeDriver //' -e 's/ (.*//' 2>/dev/null`
