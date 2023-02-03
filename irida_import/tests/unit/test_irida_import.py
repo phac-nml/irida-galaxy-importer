8339745cb954100965044f9baf685dd46f5ecc94
Deep Sidhu <deepsidhu85@gmail.com>
2023-02-03 11:28:16 -0600

    Attempting to get error message using get

--- irida_import/tests/unit/test_irida_import.py
+++ irida_import/tests/unit/test_irida_import.py
@@ -196,7 +196,7 @@ class TestIridaImport:
         try:
             sample_file.get_content_type()
         except ValueError as content_type_error:
-            assert "Unable to detect type of file and set content type" in content_type_error
+            assert "Unable to detect type of file and set content type" in content_type_error.get('message')
 
     def test_get_first_or_make_lib_empty(self, imp):
         """Test library creation if there are no preexisting libraries"""
